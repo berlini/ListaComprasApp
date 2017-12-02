7b497a38d34665c9db359fc7dc78543e1ff30799
Felipe Berlini <felipe.berlini@gmail.com>
2017-12-02 17:15:34 -0200

    Repositórios
    Adicionando início de repositórios

---
+++ ListaComprasApp/ListaComprasApp/Repositories/ItemRepository.cs
@@ -0,0 +1,29 @@
+﻿using ListaComprasApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaComprasApp.Repositories
+{
+    public class ItemRepository
+    {
+        public List<Item> LoadAll()
+        {
+            using (var context = new ListaComprasContex())
+            {
+                return context.Items.ToList();
+            }
+        }
+
+        public void Create(Item item)
+        {
+            using (var context = new ListaComprasContex())
+            {
+                context.Items.Add(item);
+                context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ ListaComprasApp/ListaComprasApp/Repositories/ListaComprasRepository.cs
@@ -0,0 +1,29 @@
+﻿using ListaComprasApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaComprasApp.Repositories
+{
+    public class ListaComprasRepository
+    {
+        public List<ListaCompras> LoadAll()
+        {
+            using (var context = new ListaComprasContex())
+            {
+                return context.ListasCompras.ToList();
+            }
+        }
+
+        public void Create(ListaCompras lista)
+        {
+            using (var context = new ListaComprasContex())
+            {
+                context.ListasCompras.Add(lista);
+                context.SaveChangesAsync();
+            }
+        }
+    }
+}
